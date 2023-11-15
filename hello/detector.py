eac6c5c53ba8b9a0eaeba2d0a69f4b63a4b2130f
uok825 <omerleroyunda@gmail.com>
2023-11-15 13:35:11 +0300

    second workshop

--- hello/detector.py
+++ hello/detector.py
@@ -1,22 +1,22 @@
 import json
 
 with open("./aleo2json.json", "r") as f:
-    aleo2json = json.load(f)
+    json_obj = json.load(f)
 
-print(f"{aleo2json}")
+print(f"{json_obj}")
 
 result = False
 
-insts = aleo2json["functions"]["main"]["instructions"]
+insts = json_obj["functions"]["main"]["instructions"]
 for inst in insts:
     tokens = inst["str"].strip(";").split()
-    #match tokens:
-     #   case ["div", r0, r1, "into", r3]:
-      #      a = b
-       # case _:
-        #    pass
+    match tokens:
+        case ["div", r0, r1, "into", r3]:
+            pass
+        case _:
+            pass
 for inst in insts:
     tokens = inst["str"].strip(";").split()
-    if tokens[0] == "div":
-        result = True
+    #if tokens[0] == "div":
+     #   result = True
 print(f"result: {result}")
